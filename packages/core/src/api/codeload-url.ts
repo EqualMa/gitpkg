f5256b9a162e7bda065c1875be9dc5872ada65dd
Ben <ben@zfben.com>
2021-12-26 10:53:48 +0800

    Throw error when missing required params

--- packages/core/src/api/codeload-url.ts
+++ packages/core/src/api/codeload-url.ts
@@ -1,2 +1,6 @@
-export const codeloadUrl = (repo: string, commit: string) =>
-  `https://codeload.github.com/${repo}/tar.gz/${commit}`;
+export function codeloadUrl (repo: string, commit: string) {
+  if (!repo) throw Error('Missing repo')
+  if (!commit) throw Error('Missing commit')
+
+  return `https://codeload.github.com/${repo}/tar.gz/${commit}`;
+}
