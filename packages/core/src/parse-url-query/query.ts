86cca8f6030efcbf041fa49be994be2700e25729
Equal Ma <equalma@outlook.com>
2024-01-17 08:42:23 +0800

    chore: refactor

--- packages/core/src/parse-url-query/query.ts
+++ packages/core/src/parse-url-query/query.ts
@@ -1,3 +1,20 @@
 export type RequestQuery = {
   [K in string]?: string | string[];
 };
+
+export function paramsToQuery(params: URLSearchParams): RequestQuery {
+  const res: RequestQuery = {};
+
+  for (const [name, value] of params) {
+    const oldValue: string | string[] | undefined = res[name];
+    if (typeof oldValue === "string") {
+      res[name] = [oldValue, value];
+    } else if (Array.isArray(oldValue)) {
+      oldValue.push(value);
+    } else {
+      res[name] = value;
+    }
+  }
+
+  return res;
+}
