063e062b4bfb4a6cbb26c90559be58294c9d8c30
Equal Ma <equalma@outlook.com>
2024-01-22 18:45:19 +0800

    fix: use readable-stream browser files

--- packages/edge-polyfill/src/stream.ts
+++ packages/edge-polyfill/src/stream.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./shims.d.ts" />
 
-import stream from "readable-stream";
+import stream from "readable-stream/lib/ours/browser";
 
 const { pipeline } = stream.promises;
 export { pipeline };
