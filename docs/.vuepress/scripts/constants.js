3692aaab70fb4abb2d5ea88cf33cd792e832a7d8
Equal Ma <equalma@outlook.com>
2024-01-22 11:40:59 +0800

    feat: website pwa

--- docs/.vuepress/scripts/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-const path = require("path");
-exports.SITE_META_FILE = path.join(__dirname, "../gen/site-meta.json");
-
-exports.PATH_DEST = path.join(__dirname, "../../../public");
